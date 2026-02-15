19f360c17e87c5141fdf70ed425bc507d953a622
Rosali <rosali.alfta@gmail.com>
2026-02-15 23:58:32 +0100

    refactor: remove redundant code and improve server initialization

--- server.js
+++ server.js
@@ -43,27 +43,6 @@ server.route({
 
   // Test route
   server.route({
-  const server = Hapi.server({
-    port: process.env.PORT || 5000,
-    host: '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['http://localhost:5173', 'https://your-netlify-app.netlify.app'],
-        credentials: false // using JWT in localStorage, no cookies needed
-      }
-    }
-  });
-
-  // Connect to MongoDB
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("Connected to database"))
-    .catch(err => console.log("Database connection error:", err));
-
-  // Register all routes
-  routes(server);
-
-  // Test route
-  server.route({
     method: 'GET',
     path: '/',
     handler: () => 'The server is up and running!'
@@ -71,18 +50,10 @@ server.route({
 
   await server.start();
   console.log("Server running on", server.info.uri);
-    handler: () => 'Server is up and running!'
-  });
-
-  await server.start();
-  console.log('Server running on', server.info.uri);
 };
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
-  process.exit(1);
-process.on('unhandledRejection', err => {
-  console.error(err);
   process.exit(1);
 });
 
